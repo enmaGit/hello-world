3bd9a993f933fd31ddda3c284148867e1130f6de
enmaGit <enmanuel21498@gmail.com>
2016-05-30 13:41:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/hustling04/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.hustling04.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.inputName)
+    EditText editName;
+
+    @Bind(R.id.btnSaludar)
+    Button btnSaludar;
+
+    @Bind(R.id.labelSaludo)
+    TextView txtSaludo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSaludar)
+    public void mostrarSaludo() {
+        if (editName.getText().toString().trim().length() > 0) {
+            txtSaludo.setText(getString(R.string.saludo, editName.getText()));
+        } else {
+            Toast.makeText(MainActivity.this, "Por favor, introduce tu nombre", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
